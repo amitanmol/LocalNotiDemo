45767c259ae30259e5f141c71a624ff4a3c4aab4
Amit Kumar Singh <amit.aanmol@gmail.com>
2019-01-18 08:25:06 +0530

    Initial Commit

---
+++ LocalNotiDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  LocalNotiDemo
+//
+//  Created by Amit Kumar Singh on 18/01/19.
+//  Copyright © 2019 Amit Kumar Singh. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonCreateNotification(_ sender: Any) {
+        //creating the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Title"
+        content.body = "Body"
+        content.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "TestIdentifier", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+}
+
