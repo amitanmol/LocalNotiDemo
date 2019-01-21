b06b5c236f258ddf7f5e37c521c7e27ae365452c
Amit Kumar Singh <amit.aanmol@gmail.com>
2019-01-21 08:42:36 +0530

    - Multiple notifications. - Read pending notifications.

--- LocalNotiDemo/ViewController.swift
+++ LocalNotiDemo/ViewController.swift
@@ -11,6 +11,8 @@ import UserNotifications
 
 class ViewController: UIViewController {
 
+    let center = UNUserNotificationCenter.current()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,14 +21,31 @@ class ViewController: UIViewController {
     @IBAction func buttonCreateNotification(_ sender: Any) {
         //creating the notification content
         let content = UNMutableNotificationContent()
-        content.title = "Title"
-        content.body = "Body"
+        content.title = "Title1"
+        content.body = "Body1"
         content.sound = .default
+        content.userInfo = ["User": "User1"]
         
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        var trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        var request = UNNotificationRequest(identifier: "NotificationIdentifier", content: content, trigger: trigger)
+        center.add(request, withCompletionHandler: nil)
         
-        let request = UNNotificationRequest(identifier: "NotificationIdentifier", content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        content.title = "Title2"
+        content.body = "Body2"
+        content.userInfo = ["User": "User2"]
+        trigger = UNTimeIntervalNotificationTrigger(timeInterval: 15, repeats: false)
+        request = UNNotificationRequest(identifier: "NotificationIdentifier1", content: content, trigger: trigger)
+        center.add(request, withCompletionHandler: nil)
+    }
+    
+    @IBAction func buttonReadNotifications(_ sender: Any) {
+        // Find list of Local Notification the app has already set
+        center.getPendingNotificationRequests { (notifications) in
+            print("Count: \(notifications.count)")
+            for item in notifications {
+                print("\(item.content.userInfo) \n")
+            }
+        }
     }
 }
 
